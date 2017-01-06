260ebe17f53eebf8004fed7f79106dece4902fc2
Blaise Bernier <sonoblaise@gmail.com>
2017-01-06 15:39:10 -0500

    First Commit

---
+++ CGSize+MyExtensions.swift
@@ -0,0 +1,16 @@
+//
+//  CGSize+MyExtensions.swift
+//  Fly Solar
+//
+//  Created by Blaise Bernier on 2016-12-14.
+//  Copyright © 2016 Badger Studios. All rights reserved.
+//
+
+import SpriteKit
+
+extension CGSize {
+   init(square: CGFloat) {
+      self.width = square
+      self.height = square
+   }
+}
